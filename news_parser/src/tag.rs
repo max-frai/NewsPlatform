263c80c7e7429c2faae6b6611a5b723a20b3b6f6
Max Frai <Ockonal@gmail.com>
2020-11-19 18:17:20 +0200

    Find in wikipedia for tags

--- news_parser/src/tag.rs
+++ news_parser/src/tag.rs
@@ -1,4 +1,5 @@
 use duct::*;
+use lazy_static::lazy_static;
 use maplit::hashmap;
 use mongodb::{
     bson::{doc, document::Document, Bson},
@@ -12,6 +13,7 @@ use std::env;
 use std::io::Write;
 use std::process::{Command, Stdio};
 use std::{collections::HashMap, sync::Arc};
+use three_set_compare::ThreeSetCompare;
 use unicode_segmentation::UnicodeSegmentation;
 
 use bson::oid::ObjectId;
@@ -37,24 +39,63 @@ pub struct ClusteringThread {
     pub category: String,
 }
 
-fn ner(text: &str) -> anyhow::Result<(Vec<String>, Vec<String>)> {
+fn _ner(chunks: Vec<String>) -> anyhow::Result<(Vec<String>, Vec<String>)> {
     let client = reqwest::blocking::Client::new();
     let result = client
         .post("http://localhost:5555/model")
         .json(&maplit::hashmap! {
-            "x" => vec![text]
+            "x" => chunks
         })
         .send()?
         .json::<Vec<Vec<Vec<String>>>>()?;
 
-    Ok((result[0][0].to_owned(), result[0][1].to_owned()))
+    let mut final_words = vec![];
+    let mut final_tags = vec![];
+
+    for pairs in result.iter() {
+        final_words.append(&mut pairs[0].to_owned());
+        final_tags.append(&mut pairs[1].to_owned());
+    }
+
+    Ok((final_words, final_tags))
+}
+
+fn ner(text: &str) -> anyhow::Result<(Vec<String>, Vec<String>)> {
+    let chars =
+        unicode_segmentation::UnicodeSegmentation::graphemes(text, true).collect::<Vec<&str>>();
+
+    // TODO: Don't split on words (use commas, dots, newlines)
+    let mut chunks: Vec<String> = vec![];
+    for chunk in chars.chunks(1000) {
+        chunks.push(chunk.iter().map(|i| i.to_string()).collect());
+    }
+
+    _ner(chunks)
+}
+
+lazy_static! {
+    static ref OK_TAGS: Vec<&'static str> = vec![
+        "person",
+        "norp",
+        "organization",
+        "gpe",
+        "event",
+        "law",
+        "product",
+        "facility",
+    ];
+    // static ref TAG_TO_DESC: HashMap<&'static str, &'static str> = hashmap! {
+    //     "gpe" => "страна",
+    //     "person" => "человек",
+    //     "organization" => "организация"
+    // };
 }
 
 pub fn tag_news(client: Arc<Client>) {
     let db = client.database("news");
     let news_collection = db.collection("news");
 
-    let options = FindOptions::builder().limit(50).build();
+    let options = FindOptions::builder().limit(3).build();
     let news = news_collection
         .find(
             Some(doc! {
@@ -78,24 +119,26 @@ pub fn tag_news(client: Arc<Client>) {
     let client = reqwest::blocking::Client::new();
 
     for item in news {
-        let text = item.get("markdown").unwrap().as_str().unwrap();
+        let text = item
+            .get("markdown")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .replace("*", "");
+
         let _id = item.get("_id").unwrap().as_object_id().unwrap();
 
-        let (words, tags) = ner(text).unwrap();
+        println!("Text:\n{}", text.trim());
+
+        let (words, tags) = ner(&text.trim()).unwrap_or((vec![], vec![]));
+
+        if words.is_empty() {
+            println!("No ner words, skip");
+            continue;
+        }
 
         let mut words = words.iter();
         let mut tags = tags.iter();
-
-        let ok_tags = vec![
-            "person",
-            "norp",
-            "organization",
-            "gpe",
-            "event",
-            "law",
-            "product",
-            "facility",
-        ];
 
         let mut current_word = String::new();
         let mut passed_words = vec![];
@@ -111,11 +154,14 @@ pub fn tag_news(client: Arc<Client>) {
 
             // println!("{} - {}", word, tag);
 
-            for ok in &ok_tags {
+            for ok in OK_TAGS.iter() {
                 if tag.ends_with(&format!("-{}", ok)) {
                     if tag.starts_with("b-") {
                         if !current_word.is_empty() {
-                            passed_words.push([current_word, previous_tag]);
+                            passed_words.push((
+                                current_word,
+                                previous_tag.replace("i-", "").replace("b-", ""),
+                            ));
                             current_word = String::new();
                         }
                         current_word = word.to_owned();
@@ -129,11 +175,67 @@ pub fn tag_news(client: Arc<Client>) {
             }
         }
 
-        dbg!(passed_words);
+        // dbg!(passed_words);
+
+        let mut wiki = Wiki::default();
+        wiki.language = "ru".to_owned();
+        wiki.search_results = 1;
 
         pub type Wiki = wikipedia::Wikipedia<wikipedia::http::default::Client>;
 
+        let brackets_re = regex::Regex::new(r"\(.*?\)").unwrap();
+        let mut tags = vec![];
+        let mut already_searched = vec![];
+
+        let comparator = ThreeSetCompare::new();
+
+        for pair in &passed_words {
+            let word = &pair.0;
+            let tag = &pair.1;
+
+            // let helper = TAG_TO_DESC.get(tag.as_str()).unwrap_or(&"");
+            // let query = if helper.is_empty() {
+            //     word.to_owned()
+            // } else {
+            //     format!("{} {}", word, helper)
+            // };
+
+            if already_searched.contains(&word) {
+                continue;
+            }
+
+            // println!("Search wiki for: {}; {}", word, tag);
+            let search_result = wiki.search(&word).unwrap();
+            if let Some(found) = search_result.first() {
+                let mut found = found.to_owned();
+                found = found.replace(",", "");
+                found = brackets_re.replace_all(&found, "").to_string();
+                println!("Found: {}; Original: {}", found, word);
+                let similarity = comparator.similarity(&found, word);
+                // dbg!(similarity);
+                // println!("---------");
+
+                if !tags.contains(&found) && similarity > 0.5 {
+                    tags.push(found);
+                }
+            }
+
+            already_searched.push(word);
+        }
+
+        dbg!(&tags);
+
+        // news_collection.update_one(
+        //     doc! {
+        //         "_id" : _id
+        //     },
+        //     doc! {
+        //         "$set" : doc!{ "tags" : tags }
+        //     },
+        //     None,
+        // );
+
         // dbg!(model_response);
-        break;
+        // break;
     }
 }
