212bdf7133b65dd2cbef40cae827ff2a362db2cc
Max Frai <me@maxfrai.com>
2020-12-14 17:52:50 +0200

    Increase xvfb size

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ rm -rf /tmp/*
 echo "Wait before create Xvfb"
 sleep 3
 
-nohup Xvfb :42 -screen 0 1024x768x16 &
+nohup Xvfb :42 -screen 0 1920x1024x16 &
 
 echo "Xvfb pid: "
 echo $!
@@ -17,7 +17,7 @@ echo $!
 echo "Wait after crate Xvfb"
 sleep 5
 
-#export DISPLAY=:42
+export DISPLAY=:42
 #DISPLAY=:42 ./news_parser
 
 # nohup ./news_server > server_LOG &
