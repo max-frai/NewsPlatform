819df648bc14b3f9f51545120efe546646b19231
Max Frai <me@maxfrai.com>
2021-02-15 00:36:58 +0200

    Run script

--- run.sh
+++ run.sh
@@ -19,9 +19,14 @@ sleep 5
 
 export DISPLAY=:42
 
-#echo "RUN PARSER"
-#nohup ./news_parser > parser_LOG &
-#echo "RUN SERVER"
-#./news_server &>> server_LOG
-#python3 news_ner.py
-sleep 1000000
+echo "RUN NER"
+nohup python3 news_ner.py > ner_LOG &
+
+echo "RUN WEBSOCKET"
+nohup ./news_websocket > websocket_LOG &
+
+echo "RUN PARSER"
+nohup ./news_parser > parser_LOG &
+
+echo "RUN SERVER"
+./news_server
