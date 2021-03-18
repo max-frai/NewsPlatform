9a53eac427695bf84492ae1c5d8f766e36622ee0
Max Frai <me@maxfrai.com>
2021-03-18 17:24:55 +0200

    Robots update

--- news_server/src/routes/robots.rs
+++ news_server/src/routes/robots.rs
@@ -3,7 +3,7 @@ use actix_web::{get, web, HttpResponse, Responder};
 
 #[get("/robots.txt")]
 async fn robots(state: web::Data<State>) -> impl Responder {
-    let sitemap_url = format!("{}/sitemap.xml", state.constants.full_domain);
+    let sitemap_url = format!("{}/head_sitemap.xml", state.constants.full_domain);
     let body = format!("User-agent: *\nDisallow:\nSitemap: {}", sitemap_url);
     HttpResponse::Ok().content_type("text/plain").body(&body)
 }
